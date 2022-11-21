a250a854ad8a27a489cda05faf12319b7246c389
Brezzor <oliv095w@edu.zealand.dk>
2022-11-21 11:43:19 +0100

    Added dbconxt and connectionstring to project

---
+++ Bookware/Models/BookwareDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bookware.Models
+{
+    public class BookwareDbContext: DbContext
+    {
+        public BookwareDbContext()
+        {
+        }
+
+        public BookwareDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
