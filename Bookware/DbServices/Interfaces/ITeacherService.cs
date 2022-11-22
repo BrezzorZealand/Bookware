2e11038111fe3891a16bd05b764716590c6437c8
Rex77pro <smaurieka@gmail.com>
2022-11-22 12:47:48 +0100

    50% Teacher CRUD

---
+++ Bookware/DbServices/Interfaces/ITeacherService.cs
@@ -0,0 +1,14 @@
+﻿using Bookware.Models;
+
+namespace Bookware.DbServices.Interfaces
+{
+    public interface ITeacherService
+    {
+        IEnumerable<Teacher> GetTeachers();
+        void AddTeacher(Teacher teacher);
+        void RemoveTeacher(Teacher teacher);
+        void EditTeacher(Teacher teacher);
+        Teacher GetTeacher(int id);
+
+    }
+}
--- Bookware/Pages/Teacher_Pages/AllTeachers.cshtml.cs
+++ Bookware/Pages/Teacher_Pages/AllTeachers.cshtml.cs
@@ -1,12 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookware.DbServices.Interfaces;
+using Bookware.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+
 
 namespace Bookware.Pages.Teacher_Pages
 {
     public class AllTeachersModel : PageModel
     {
-        public void OnGet()
+        public IEnumerable<Teacher> Teachers { get; set; }
+        public int TId { get; set; }
+        private ITeacherService context;
+
+        public AllTeachersModel(ITeacherService service)
         {
+            context = service;
+        }
+            
+        public void OnGet(int tid)
+        {
+            TId = tid;
+            Teachers = context.GetTeachers();
         }
     }
 }
