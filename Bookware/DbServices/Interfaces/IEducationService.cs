461d4501ad2a00c78e7cad948108101f8c4b48e3
Simon Søby <sb.simonzone@gmail.com>
2022-11-24 15:13:22 +0100

    Started Crud On Education
    Made AllEducations so that you can view all educations

---
+++ Bookware/DbServices/Interfaces/IEducationService.cs
@@ -0,0 +1,13 @@
+﻿using Bookware.Models;
+
+namespace Bookware.DbServices.Interfaces
+{
+    public interface IEducationService
+    {
+        void CreateEducation(Education education);
+        void DeleteEducation(Education education);
+        void EditEducation(int id);
+        Education? GetEducation(int id);
+        IEnumerable<Education> GetEducations();
+    }
+}
--- Bookware/Pages/Education_Pages/AllEducations.cshtml.cs
+++ Bookware/Pages/Education_Pages/AllEducations.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Bookware.Models;
+using Bookware.DbServices.Interfaces;
 
 namespace Bookware.Pages.Education_Pages
 {
     public class AllEducationsModel : PageModel
     {
+        public readonly IEducationService Service;
+        public IEnumerable<Education>? Educations { get; set; }
+
+        public AllEducationsModel(IEducationService Service)
+        {
+            this.Service = Service;
+        }
+        
         public void OnGet()
         {
+            Educations = Service.GetEducations();
         }
     }
 }
