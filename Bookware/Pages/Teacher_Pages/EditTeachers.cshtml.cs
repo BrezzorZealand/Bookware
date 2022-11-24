3c7090d724a67b6c7821866f1a9969b2b3876ebd
Rex77pro <smaurieka@gmail.com>
2022-11-24 13:42:14 +0100

    Teachers update

---
+++ Bookware/Pages/Teacher_Pages/EditTeachers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bookware.Pages.Teacher_Pages
+{
+    public class EditTeachersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
