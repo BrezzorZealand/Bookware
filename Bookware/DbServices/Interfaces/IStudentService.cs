57daa686e2502ec3b620d019d45e4f65750928c1
ksekmen1 <kaya.sekmen@gmail.com>
2022-11-24 16:49:39 +0100

    Student

---
+++ Bookware/DbServices/Interfaces/IStudentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Bookware.Models;
+namespace Bookware.DbServices.Interfaces
+{
+	public interface IStudentService
+	{
+        IEnumerable<Student> GetStudents();
+        Student? GetStudentById(int id);
+        void RemoveStudent(Student student);
+        void EditStudent(Student student);
+        void AddStudent(Student student);
+    }
+}
+
---
+++ Bookware/DbServices/Services/StudentService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Bookware.Models;
+using Bookware.Interfaces;
+using Bookware.DbServices.Interfaces;
+
+namespace Bookware.DbServices.Services
+{
+	public class StudentService : IStudentService
+	{
+        private readonly BookwareDbContext context;
+
+        public StudentService(BookwareDbContext context)
+        {
+            this.context = context;
+        }
+        public Student? GetStudentById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveStudent(Student student)
+        {
+            context.Students.Remove(student);
+            context.SaveChanges();
+        }
+
+        public void AddStudent(Student student)
+        {
+            context.Students.Add(student);
+            context.SaveChanges();
+        }
+
+        public void EditStudent(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
+
