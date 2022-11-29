9163bd9a03db0f9bba1652965aed8f222bf2cc84
Rex77pro <smaurieka@gmail.com>
2022-11-29 12:00:46 +0100

    Services class + oprettet pages

---
+++ Bookware/DbServices/Interfaces/IClassService.cs
@@ -0,0 +1,13 @@
+﻿using Bookware.Models;
+
+namespace Bookware.DbServices.Interfaces
+{
+    public interface IClassService
+    {
+        Task AddClassAsync(Class? klasse); 
+        Task <IEnumerable<Class>> GetClassAsync();
+        Task <Class> GetClassByIdAsync(int id);
+        Task UpdateClassAsync(Class? klasse);
+        Task DeleteClassAsync(Class? klasse);
+    }
+}
---
+++ Bookware/Pages/Class_Pages/UpdateClass.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bookware.Pages.Class_Pages
+{
+    public class UpdateClassModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
