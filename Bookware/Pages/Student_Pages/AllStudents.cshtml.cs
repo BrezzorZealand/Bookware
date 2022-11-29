e88aee4b3996c3d83197db3835bef795f4e0c3b0
ksekmen1 <kaya.sekmen@gmail.com>
2022-11-29 01:03:37 +0100

    Student start of CRUD implementation

--- Bookware/Pages/Student_Pages/AllStudents.cshtml.cs
+++ Bookware/Pages/Student_Pages/AllStudents.cshtml.cs
@@ -1,12 +1,32 @@
+using Bookware.DbServices.Interfaces;
+using Bookware.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
 
 namespace Bookware.Pages.Student_Pages
 {
     public class AllStudentsModel : PageModel
     {
-        public void OnGet()
+        public IEnumerable<Student?> Students { get; set; }
+      
+        private IStudentService context;
+
+        public AllStudentsModel(IStudentService service)
         {
+            this.context = service;
         }
+
+        [BindProperty]
+        public Student? Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            
+            Students = await context.GetStudentsAsync();
+            return Page();
+        }
+       
     }
 }
