97f905f422d8bad472dd61b29202d3128e9b9960
Rolsen <Bruger@LAPTOP-LHCL8MS0>
2022-12-01 11:39:50 +0100

    "Details" fixing

---
+++ Bookware/Pages/Subject_Pages/SubjectDetails.cshtml.cs
@@ -0,0 +1,27 @@
+using Bookware.DbServices.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Bookware.Models;
+
+namespace Bookware.Pages.Subject_Pages
+{
+    public class SubjectDetailsModel : PageModel
+    {
+        private readonly ISubjectService service;
+        public SubjectDetailsModel(ISubjectService service)
+        {
+            this.service = service;
+        }
+        public Subject? Subject { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Subject = await service.GetSubjectDataById(id);
+            if (Subject == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
