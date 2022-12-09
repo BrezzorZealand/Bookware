c1603ce14e3bfc23c1947a20c59582794449b5da
Simon Søby <sb.simonzone@gmail.com>
2022-12-09 15:45:42 +0100

    Generic changes

--- Bookware/DbServices/Services/GenericService.cs
+++ Bookware/DbServices/Services/GenericService.cs
@@ -4,21 +4,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Bookware.DbServices.Services
 {
-    public class GenericService<T>  /*IGenericService<T> where T : class*/
+    public class GenericService<T> : IGenericService<T> where T : class
     {
         private readonly BookwareDbContext context;
-        private readonly DbSet<T> dbSet;
 
         public GenericService(BookwareDbContext context)
         {
             this.context = context;
-            dbSet = context.Set<T>();
         }
 
-        public async Task<IEnumerable<T>> GetAll<T>()
+        public async Task<List<T>> GetAll<T>() where T : class
         {
-            return await dbSet
-                .AsNoTracking()
+            return await context.Set<T>()
                 .ToListAsync();
         }
 
@@ -28,9 +25,9 @@ namespace Bookware.DbServices.Services
         /// <param name="filter">The string to filter (can be NULL)</param>
         /// <param name="propertyName">Name of the Property the filter will filter </param>
         /// <returns>A list of objects that match the filter</returns>
-        public async Task<IEnumerable<T>> GetAllFilter<T>(string? filter, string? propertyName)
+        public async Task<List<T>> GetAllFilter<T>(string? filter, string? propertyName) where T : class
         {
-            return await dbSet
+            return await context.Set<T>()
                 .Where(t => t
                             .GetType()
                             .GetProperty(propertyName!)
@@ -40,35 +37,37 @@ namespace Bookware.DbServices.Services
 
         }
 
-        public async Task<T> GetById<T>(int id)
+        public async Task<T> GetById<T>(int id) where T : class
         {
-            T? entity = await dbSet.FindAsync(id);
+            T? entity = await context!.Set<T>()
+                                 .FindAsync(id);
+
             return entity!;
         }
 
-        public async Task Create<T>(T entity)
+        public async Task Create<T>(T entity) where T : class
         {
             if (entity is not null)
             {
-                await dbSet.AddAsync(entity);
+                await context.Set<T>().AddAsync(entity);
             }
             await context.SaveChangesAsync();
         }
 
-        public async Task Update<T>(T entity)
+        public async Task Update<T>(T entity) where T : class
         {
             if (entity is not null)
             {
-                dbSet.Update(entity);
+                context.Set<T>().Update(entity);
             }
             await context.SaveChangesAsync();
         }
 
-        public async Task Delete<T>(T entity)
+        public async Task Delete<T>(T entity) where T : class
         {
             if (entity is not null)
             {
-                dbSet.Remove(entity);
+                context.Set<T>().Remove(entity);
             }
             await context.SaveChangesAsync();
         }
