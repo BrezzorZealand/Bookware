e12b152a3b50a7d61209205b247ce448e64e8287
Brezzor <oliv095w@edu.zealand.dk>
2022-12-16 12:58:30 +0100

    Fixed minor errors

--- Bookware/DbServices/Services/TeacherClassService.cs
+++ Bookware/DbServices/Services/TeacherClassService.cs
@@ -30,7 +30,7 @@ namespace Bookware.DbServices.Services
             IEnumerable<SelectListItem> SelectListItems = from tc in teacherClasses
                                                           select new SelectListItem
                                                           {
-                                                              Text = tc.TeachEdu.EduSub.EduSubDisplayName,
+                                                              Text = tc.TeachEdu.EduSub.Edu.EduName,
                                                               Value = tc.TeachEduId.ToString(),
                                                               Group = selectListGroups.FirstOrDefault(slg => slg.Name == tc.TeachEdu.Teacher.Name)
                                                           };
