fa5f1731254b6f9b89c7d704d1300b05eb0a0d39
Rolsen <Bruger@LAPTOP-LHCL8MS0>
2022-12-16 15:07:24 +0100

    Got getall to work in unit test

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -8,18 +8,26 @@ using Bookware.Pages.Subject_Pages;
 using Bookware.DbServices.Interfaces;
 using System.Security.Cryptography.X509Certificates;
 
+
 namespace UnitTest
 {
     [TestClass]
     public class UnitTest1
     {
+        public readonly string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BookwareDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        
+        public BookwareDbContext GetDBcontext()
+        {
+            DbContextOptionsBuilder<BookwareDbContext> Builder = new DbContextOptionsBuilder<BookwareDbContext>()
+                .UseSqlServer(connectionString);
+            return new BookwareDbContext(Builder.Options);
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
-            BookwareDbContext context;
-            DbContextOptionsBuilder<BookwareDbContext> Builder = new DbContextOptionsBuilder<BookwareDbContext>()
-                .UseInMemoryDatabase(databaseName: "BookwareDb");
-            context = new BookwareDbContext(Builder.Options);
+            BookwareDbContext context = GetDBcontext();
+            SubjectService sut = new SubjectService(context);
 
             var mockEducation = new Education
             {
@@ -32,7 +40,7 @@ namespace UnitTest
             };
 
             // Act
-            //await sut.Create(mockEducation, mockSubject);
+            sut.Create(mockEducation, mockSubject);
 
             // Assert
             Assert.IsNotNull(mockEducation.EduSubs, "EduSubs is null");
@@ -42,33 +50,24 @@ namespace UnitTest
         public void TestGetSubjects()
         {
             //Arrange
-            BookwareDbContext context;
-            DbContextOptionsBuilder<BookwareDbContext> Builder = new DbContextOptionsBuilder<BookwareDbContext>()
-                .UseInMemoryDatabase(databaseName: "BookwareDb");
-            context = new BookwareDbContext(Builder.Options);
+            BookwareDbContext context = GetDBcontext();
 
-            SubjectService sut2 = new(context);
+            SubjectService sut2 = new SubjectService(context);
 
-            var mockSubject2 = new Subject
-            {
-                SubjectName = "TestSubject2"
-            };
+
             //Act
-            sut2.GetAll();
+            IEnumerable<Subject> subjects = sut2.GetAll();
             //Assert
 
-            Assert.IsNotNull(mockSubject2);
+            Assert.IsNotNull(subjects);
             //Assert.IsNull(subjects);
         }
         [TestMethod]
         public async Task TestRemoveSubjectAsync()
         {
-            BookwareDbContext context;
-            DbContextOptionsBuilder<BookwareDbContext> Builder = new DbContextOptionsBuilder<BookwareDbContext>()
-                .UseInMemoryDatabase(databaseName: "BookwareDb");
-            context = new BookwareDbContext(Builder.Options);
+            BookwareDbContext context = GetDBcontext();
 
-            SubjectService sut3 = new(context);
+            SubjectService sut3 = new SubjectService(context);
             Subject Last_subject;
             Subject Last_subject2;
 
@@ -83,17 +82,17 @@ namespace UnitTest
             };
             
             
+            var test = sut3.GetAll();
             Last_subject = sut3.GetAll().OrderBy(x => x.SubjectId).Last();
             Subject last1 = Last_subject;
 
             await sut3.Create(mockSubject3);
-
             Last_subject2 = sut3.GetAll().OrderBy(x => x.SubjectId).Last();
             Subject last2 = Last_subject2;
 
             //await sut3.Delete(mockSubject3);
 
-            Assert.AreEqual(last1, last2);
+            Assert.AreNotEqual(last1, last2);
             //if (mockSubject3 != null)
             //{
             //  await sut3.RemoveSubjectAsync(mockSubject3);
