23f29180a237bb0b226a99059d2829ac7cfad3d4
Rolsen <Bruger@LAPTOP-LHCL8MS0>
2022-12-19 11:42:27 +0100

    Unit test done - Update now testable

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -15,7 +15,9 @@ namespace UnitTest
     public class UnitTest1
     {
         public readonly string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BookwareDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-        
+
+        public string? SubjectName { get; private set; }
+
         public BookwareDbContext GetDBcontext()
         {
             DbContextOptionsBuilder<BookwareDbContext> Builder = new DbContextOptionsBuilder<BookwareDbContext>()
@@ -24,15 +26,10 @@ namespace UnitTest
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public async Task TestCreate()
         {
             BookwareDbContext context = GetDBcontext();
             SubjectService sut = new SubjectService(context);
-
-            var mockEducation = new Education
-            {
-                EduName = "TestEdu"
-            };
 
             var mockSubject = new Subject
             {
@@ -40,14 +37,14 @@ namespace UnitTest
             };
 
             // Act
-            //sut.Create(mockEducation, mockSubject);
+            await sut.Create(mockSubject);
 
             // Assert
-            Assert.IsNotNull(mockEducation.EduSubs, "EduSubs is null");
+            Assert.IsNotNull(mockSubject);
         }
 
         [TestMethod]
-        public void TestGetSubjects()
+        public void TestGetAll()
         {
             //Arrange
             BookwareDbContext context = GetDBcontext();
@@ -74,44 +71,46 @@ namespace UnitTest
 
             Subject mockSubject3 = new Subject
             {
-                SubjectName = "TestSubjecttesttest"
-            };
-            Subject mockSubject4 = new Subject
-            {
-                SubjectName = "TestSubject"
+                SubjectName = "testsubremove"
             };
             
-            
-            var test = sut3.GetAll();
             Last_subject = sut3.GetAll().OrderBy(x => x.SubjectId).Last();
             Subject last1 = Last_subject;
 
             await sut3.Create(mockSubject3);
+
+            await sut3.Delete(mockSubject3);
+
             Last_subject2 = sut3.GetAll().OrderBy(x => x.SubjectId).Last();
             Subject last2 = Last_subject2;
 
-            //await sut3.Delete(mockSubject3);
-
             Assert.AreNotEqual(last1, last2);
-            //if (mockSubject3 != null)
-            //{
-            //  await sut3.RemoveSubjectAsync(mockSubject3);
-            //}
-            //try
-            //{
-            //    Assert.AreSame(mockSubject3, await sut3.GetSubjectByIdAsync(mockSubject3.SubjectId));
-                
-            //}
-            //catch (Exception ex)
-            //{
-            //    Assert.Fail(ex.Message);
-            //}
         }
         [TestMethod]
-        public void TestEditSubjectAsync()
+        public async Task TestUpdateSubject()
         {
             BookwareDbContext context = GetDBcontext();
             SubjectService sut4 = new(context);
+            string SubjectName = "TestEdit";
+
+            //Subject mockSubject4ToUpdate = new Subject
+            //{
+            //    this.SubjectName = SubjectName
+            //};
+
+            Subject? mockSubject4ToUpdate = new Subject { SubjectName = SubjectName };
+
+            await sut4.Create(mockSubject4ToUpdate);
+
+            mockSubject4ToUpdate = sut4.GetAll().AsNoTracking().FirstOrDefault(sub => sub.SubjectName == mockSubject4ToUpdate.SubjectName);
+            mockSubject4ToUpdate!.SubjectName = "SaveEdit";
+
+            if (mockSubject4ToUpdate != null)
+            {
+                await sut4.Update(mockSubject4ToUpdate);
+            };
+
+            Assert.AreNotEqual(mockSubject4ToUpdate!.SubjectName, SubjectName);
 
         }
     }
