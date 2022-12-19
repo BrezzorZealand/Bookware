512900c12650639982d82ae110d77d820d7ad698
Simon Søby <sb.simonzone@gmail.com>
2022-12-19 14:21:16 +0100

    using change

--- Bookware/Pages/Class_Pages/ClassAssignTeacher.cshtml.cs
+++ Bookware/Pages/Class_Pages/ClassAssignTeacher.cshtml.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Bookware.Models;
 using Bookware.DbServices.Interfaces;
-using Bookware.DbServices.Services;
+
 
 namespace Bookware.Pages.Class_Pages
 {
